22ee8cbb286b99daebd90ea3d43aad6906fc14c2
Richmond Liew <richmond.liew.97@gmail.com>
2020-07-31 21:07:08 -0400

    working in minimax()

---
+++ Project0/tictactoe/test_tictactoe.py
@@ -0,0 +1,154 @@
+import tictactoe as ttt
+import pytest
+import copy
+
+EMPTY = None
+
+
+def test_winner_horizontal1():
+    board = [["X", "X", "X"],
+             [EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY]]
+    assert ttt.winner(board) == "X"
+    board = [["X", "X", "O"],
+             [EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY]]
+    assert ttt.winner(board) == None
+
+
+def test_winner_diagonal1():
+    board = [["X", EMPTY, "X"],
+             [EMPTY, "X", EMPTY],
+             [EMPTY, EMPTY, "X"]]
+    assert ttt.winner(board) == "X"
+
+
+def test_empty_winner():
+    board = [[EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY]]
+    assert ttt.winner(board) == None
+
+
+def test_terminal_empty():
+    board = [[EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY]]
+    assert ttt.terminal(board) == False
+
+
+def test_hasMoves():
+    board = [[EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY]]
+    assert ttt.hasMoves(board) == True
+
+
+def test_player_empty_board():
+    board = [[EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY]]
+    assert ttt.player(board) == "X"
+
+
+def test_player_non_empty_1():
+    board = [["X", EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY]]
+    assert ttt.player(board) == "O"
+
+
+def test_player_non_empty_2():
+    board = [["X", "O", EMPTY],
+             [EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY]]
+    assert ttt.player(board) == "X"
+
+
+def test_player_non_empty_3():
+    board = [["X", EMPTY, "X"],
+             ["O", "X", "O"],
+             ["X", "O", "X"]]
+    assert ttt.player(board) == "O"
+
+
+def test_actions_empty_board():
+    board = [[EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY],
+             [EMPTY, EMPTY, EMPTY]]
+    expected = set()
+    for y in range(0, 3):
+        for x in range(0, 3):
+            expected.add((y, x))
+    assert ttt.actions(board) == expected
+
+
+def test_actions_one_move_left():
+    board = [["X", EMPTY, "X"],
+             ["O", "X", "O"],
+             ["X", "O", "X"]]
+    expected = set()
+    expected.add((0, 1))
+    assert ttt.actions(board) == expected
+
+
+def test_utiltity_X_wins():
+    board = [["X", "O", EMPTY],
+             ["O", "X", "O"],
+             ["X", EMPTY, "X"]]
+    assert ttt.utility(board) == 1
+
+
+def test_utiltity_O_wins():
+    board = [["X", "O", "X"],
+             ["O", "O", "O"],
+             ["X", EMPTY, "X"]]
+    assert ttt.utility(board) == -1
+
+
+def test_utiltity_nobody_wins():
+    board = [["X", "O", "O"],
+             ["O", "X", "X"],
+             ["X", "X", "O"]]
+    assert ttt.utility(board) == 0
+
+
+def test_result():
+    board = [["X", "O", "X"],
+             ["O", "O", "X"],
+             ["X", EMPTY, "O"]]
+    possibleMoves = ttt.actions(board)
+    expected = [["X", "O", "X"],
+                ["O", "O", "X"],
+                ["X", "X", "O"]]
+    initBoard = copy.deepcopy(board)
+    assert ttt.result(board, possibleMoves.pop()) == expected
+    assert board == initBoard
+
+
+def test_result_throws_error():
+    board = [["X", "O", "X"],
+             ["O", "O", "X"],
+             ["X", EMPTY, "O"]]
+    with pytest.raises(ValueError):
+        ttt.result(board, (1, 1))
+
+
+def test_max_value_base_case():
+    board = [["X", "O", "X"],
+             ["O", "O", "O"],
+             ["X", EMPTY, "X"]]
+    assert ttt.max_value(board) == -1
+
+
+def test_max_value():
+    board = [["X", EMPTY, "X"],
+            ["O", "O", "X"],
+            [EMPTY, "X", "O"]]
+    assert ttt.max_value(board) == 1
+
+def test_min_value():
+    board = [["X", EMPTY, "X"],
+             ["O", "O", "X"],
+             [EMPTY, "X", "O"]]
+    assert ttt.min_value(board) == 0
--- Project0/tictactoe/tictactoe.py
+++ Project0/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,46 +23,177 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    moves = {
+        X: 0,
+        O: 0
+    }
+    for x in range(0, 3):
+        for y in range(0, 3):
+            player = board[y][x]
+            if player != EMPTY:
+                moves[player] += 1
+
+    if moves[X] > moves[O]:
+        return O
+    else:
+        return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possibleActions = set()
+    for y in range(0, 3):
+        for x in range(0, 3):
+            if board[y][x] == EMPTY:
+                possibleActions.add((y, x))
+    return possibleActions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    # Check for inavlid moves
+    if board[action[0]][action[1]] != None:
+        raise ValueError
+    resultingBoard = copy.deepcopy(board)
+    resultingBoard[action[0]][action[1]] = player(board)
+    return resultingBoard
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    row = [
+        {
+            X: 0,
+            O: 0
+        },
+        {
+            X: 0,
+            O: 0
+        },
+        {
+            X: 0,
+            O: 0
+        }
+    ]
+    col = [
+        {
+            X: 0,
+            O: 0
+        },
+        {
+            X: 0,
+            O: 0
+        },
+        {
+            X: 0,
+            O: 0
+        }
+    ]
+    diag = [
+        {
+            X: 0,
+            O: 0
+        },
+        {
+            X: 0,
+            O: 0
+        }
+    ]
+    neighbors = {
+        0: {
+            0: [row[0], col[0], diag[0]],
+            1: [row[0], col[1]],
+            2: [row[0], col[2], diag[1]]
+        },
+        1: {
+            0: [row[1], col[0]],
+            1: [row[1], col[1], diag[0], diag[1]],
+            2: [row[1], col[2]]
+        },
+        2: {
+            0: [row[2], col[0], diag[1]],
+            1: [row[2], col[1]],
+            2: [row[2], col[2], diag[0]]
+        }
+    }
+
+    for x in range(0, 3):
+        for y in range(0, 3):
+            player = board[x][y]
+            if player != EMPTY:
+                counters = neighbors[x][y]
+                for counter in counters:
+                    counter[player] += 1
+                    if counter[player] == 3:
+                        return player
+
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) != None or not hasMoves(board):
+        return True
+    else:
+        return False
+
+
+def hasMoves(board):
+    """
+    Returns True is there are any valid moves in the board, False otherwise.
+    """
+    for y in range(0, 3):
+        for x in range(0, 3):
+            if board[y][x] == EMPTY:
+                return True
+    return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if player(board) == X:
+        # Max player
+        min_value(board)
+    elif player(board) == O:
+        # Min player
+        max_value(board)
+
+
+def max_value(board):
+    if terminal(board) == True:
+        return utility(board)
+    v = -math.inf
+    for action in actions(board):
+        v = max(v, min_value(result(board, action)))
+    return v
+
+
+def min_value(board):
+    if terminal(board) == True:
+        return utility(board)
+    v = math.inf
+    for action in actions(board):
+        v = min(v, max_value(result(board, action)))
+    return v
