a5744f795bb8d2038499f4766adc43aef1ef8116
Richmond Liew <richmond.liew.97@gmail.com>
2020-08-05 15:19:34 -0400

    finished puzzle.py

--- Project1/knights/puzzle.py
+++ Project1/knights/puzzle.py
@@ -11,31 +11,55 @@ CKnave = Symbol("C is a Knave")
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
+aSays = And(AKnight, AKnave)
 knowledge0 = And(
-    # TODO
-)
+    Biconditional(Not(aSays), AKnave),
+    Biconditional(aSays, AKnight)
+    )
+knowledge0.add(Not(aSays))
+
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
+aSays = And(AKnave, BKnave)
 knowledge1 = And(
-    # TODO
+    Biconditional(Not(aSays), AKnave),
+    Biconditional(aSays, AKnight)
 )
+knowledge1.add(Not(aSays))
+knowledge1.add(Or(And(AKnight, BKnave), And(BKnight, AKnave)))
+
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+aSays = Or(And(AKnave,BKnave), And(AKnight, BKnight))
+bSays = Or(And(AKnave, BKnight), And(AKnight, BKnave))
 knowledge2 = And(
-    # TODO
+    Biconditional(Not(aSays), AKnave),
+    Biconditional(aSays, AKnight),
+    Biconditional(Not(bSays), BKnave),
+    Biconditional(bSays, BKnight),
 )
+knowledge2.add(Not(And(AKnave, BKnave, aSays)))
+knowledge2.add(Not(And(BKnave, AKnight, bSays)))
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
 # B says "A said 'I am a knave'."
 # B says "C is a knave."
 # C says "A is a knight."
+aSays = Or(AKnight, AKnave)
+bSays = And(CKnave, aSays, AKnight)
+cSays = AKnight
 knowledge3 = And(
-    # TODO
+    Biconditional(Not(aSays), AKnave),
+    Biconditional(aSays, AKnight),
+    Biconditional(Not(bSays), BKnave),
+    Biconditional(bSays, BKnight),
+    Biconditional(Not(cSays), CKnave),
+    Biconditional(cSays, CKnight)
 )
 
 
