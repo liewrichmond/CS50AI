ac8bb27bf2cd21c3de1a584c9872ecff4b3eef2f
Richmond Liew <richmond.liew.97@gmail.com>
2020-08-13 16:45:46 -0400

    implemented and tested all functions, ready for submission

--- Project2/heredity/heredity.py
+++ Project2/heredity/heredity.py
@@ -139,59 +139,57 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
-    parents = dict()
-    children = dict()
+    toCalculate = dict()
 
-    # sort children and parents
+    # retreive information about join probability
     for person in people:
         node = {
-            "gene" : (1 if person in one_gene else
-                      2 if person in two_genes else 0),
-            "trait" : (True if person in have_trait else False)
+            "gene": (1 if person in one_gene else
+                     2 if person in two_genes else 0),
+            "trait": (True if person in have_trait else False),
+            "mother": people[person]["mother"],
+            "father": people[person]["father"]
         }
-        if people[person]['mother'] == None and people[person]['father'] == None:
-            parents[person] = node
-        else:
-            node["mother"] = people[person]['mother']
-            node["father"] = people[person]['father']
-            children[person] = node
+        toCalculate[person] = node
 
-    # calculate probability for parents
-    for parent in parents:
-        n_gene = parents[parent]["gene"]
-        p_gene = PROBS["gene"][n_gene]
-        p_trait = PROBS["trait"][n_gene][parents[parent]["trait"]]
-        parents[parent]["probability"] = p_gene * p_trait
-
-    # calculate probability for kids
-    for child in children:
-        n_gene = children[child]["gene"]
-        mother = children[child]["mother"]
-        father = children[child]["father"]
-
-        p_transfer_m = (0.99 if parents[mother]["gene"] != 0 else 0.01)
-        p_no_transfer_m = 1 - p_transfer_m
-        p_transfer_f = (0.99 if parents[father]["gene"] != 0 else 0.01)
-        p_no_transfer_f = 1 - p_transfer_f
-
-        p_gene = 0
-        if n_gene == 0:
-            p_gene = p_no_transfer_m * p_no_transfer_f
-        elif n_gene == 1:
-            p_gene = (p_no_transfer_f * p_transfer_m) + \
-                (p_transfer_f * p_no_transfer_m)
-        else:
-            p_gene = p_transfer_f * p_transfer_m
-
-        p_trait = PROBS["trait"][n_gene][children[child]["trait"]]
-        children[child]["probability"] = p_gene * p_trait
-
-    # calculate joint probabilities
     probability = 1
-    for parent in parents:
-        probability *= parents[parent]["probability"]
-    for child in children:
-        probability *= children[child]["probability"]
+    for person in toCalculate:
+        # calculate probability of gene
+        if toCalculate[person]["mother"] == None and toCalculate[person]["father"] == None:
+            # do unconditionals
+            n_gene = toCalculate[person]["gene"]
+            p_gene = PROBS["gene"][n_gene]
+        else:
+            # do conditionals
+            n_gene = toCalculate[person]["gene"]
+            mother = toCalculate[person]["mother"]
+            father = toCalculate[person]["father"]
+
+            p_transfer_m = (0.99 if toCalculate[mother]["gene"] == 2 else
+                            0.5 if toCalculate[mother]["gene"] == 1 else
+                            0.01)
+            p_no_transfer_m = 1 - p_transfer_m
+            p_transfer_f = (0.99 if toCalculate[father]["gene"] == 2 else
+                            0.5 if toCalculate[father]["gene"] == 1 else
+                            0.01)
+            p_no_transfer_f = 1 - p_transfer_f
+
+            p_gene = 0
+            if n_gene == 0:
+                p_gene = p_no_transfer_m * p_no_transfer_f
+            elif n_gene == 1:
+                p_gene = (p_no_transfer_f * p_transfer_m) + \
+                    (p_transfer_f * p_no_transfer_m)
+            else:
+                p_gene = p_transfer_f * p_transfer_m
+                
+        # calculate probability of trait
+        has_trait = toCalculate[person]["trait"]
+        p_trait = PROBS["trait"][n_gene][has_trait]
+
+        #multiply final probability
+        probability *= p_trait * p_gene
+
     return probability
 
 
@@ -202,7 +200,18 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+    for person in probabilities:
+        if person in one_gene:
+            probabilities[person]["gene"][1] += p
+        elif person in two_genes:
+            probabilities[person]["gene"][2] += p
+        else:
+            probabilities[person]["gene"][0] += p
+
+        if person in have_trait:
+            probabilities[person]["trait"][True] += p
+        else:
+            probabilities[person]["trait"][False] += p
 
 
 def normalize(probabilities):
@@ -210,7 +219,18 @@ def normalize(probabilities):
     Update `probabilities` such that each probability distribution
     is normalized (i.e., sums to 1, with relative proportions the same).
     """
-    raise NotImplementedError
+    for person in probabilities:
+        s_g = sum(probabilities[person]["gene"].values())
+        s_t = sum(probabilities[person]["trait"].values())
+
+        alpha_g = 1/s_g
+        alpha_t = 1/s_t
+
+        for p_gene in probabilities[person]["gene"]:
+            probabilities[person]["gene"][p_gene] *= alpha_g
+
+        for p_trait in probabilities[person]["trait"]:
+            probabilities[person]["trait"][p_trait] *= alpha_t
 
 
 if __name__ == "__main__":
--- Project2/heredity/test_heredity.py
+++ Project2/heredity/test_heredity.py
@@ -1,12 +1,106 @@
 import pytest
+import math
 from heredity import *
 
 def test_test():
     assert 1==1
 
-def test_heredity_returns_zero_prob():
+def test_joint_probability_family_0():
     people = load_data("Project2//heredity//data//family0.csv")
     actual = joint_probability(people, {"Harry"}, {"James"}, {"James"})
     expected = 0.0026643247488
-    assert expected == actual
+    actual = joint_probability(people, {"Lily"}, {"James"}, {"James"})
+    expected = 8.4942*10**-9
+    larger = max(expected, actual)
+    smaller = min(expected, actual)
+    assert larger-smaller <= 0.001
 
+def test_update():
+    people = load_data("Project2//heredity//data//family0.csv")
+    p = joint_probability(people, {"Harry"}, {"James"}, {"James"})
+    probabilities = {
+        person: {
+            "gene": {
+                2: 0,
+                1: 0,
+                0: 0
+            },
+            "trait": {
+                True: 0,
+                False: 0
+            }
+        }
+        for person in people
+    }
+    update(probabilities, {"Harry"}, {"James"}, {"James"}, p)
+    assert probabilities["Harry"]["gene"][1] == p
+    assert probabilities["James"]["gene"][2] == p
+    assert probabilities["Lily"]["gene"][0] == p
+
+    assert probabilities["Lily"]["trait"][False] == p
+    assert probabilities["Harry"]["trait"][False] == p
+    assert probabilities["James"]["trait"][True] == p
+
+def test_update_2():
+    people = load_data("Project2//heredity//data//family0.csv")
+    p = joint_probability(people, {"Lily"}, {"James"}, {"James"})
+    expected = 4.2471*10**-7
+    probabilities = {
+        person: {
+            "gene": {
+                2: 0,
+                1: 0,
+                0: 0
+            },
+            "trait": {
+                True: 0,
+                False: 0
+            }
+        }
+        for person in people
+    }
+    update(probabilities,  {"Lily"}, {"James"}, {"James"}, p)
+    assert math.isclose(probabilities["Harry"]["gene"][0],expected)
+    assert math.isclose(probabilities["Harry"]["gene"][1],0)
+    assert math.isclose(probabilities["Harry"]["gene"][2],0)
+
+    assert math.isclose(probabilities["James"]["gene"][0], 0)
+    assert math.isclose(probabilities["James"]["gene"][1], 0)
+    assert math.isclose(probabilities["James"]["gene"][2], expected)
+
+    assert math.isclose(probabilities["Lily"]["gene"][0], 0)
+    assert math.isclose(probabilities["Lily"]["gene"][1], expected)
+    assert math.isclose(probabilities["Lily"]["gene"][2], 0)
+
+    assert math.isclose(probabilities["Lily"]["trait"][True], 0)
+    assert math.isclose(probabilities["Lily"]["trait"][False], expected)
+
+    assert math.isclose(probabilities["Harry"]["trait"][True], 0)
+    assert math.isclose(probabilities["Harry"]["trait"][False], expected)
+    
+    assert math.isclose(probabilities["James"]["trait"][True], expected)
+    assert math.isclose(probabilities["James"]["trait"][False], 0)
+
+def test_normalize():
+    probabilities = {
+        "Harry": {
+            "gene": {
+                2: 0.1,
+                1: 0.2,
+                0: 0.1
+            },
+            "trait": {
+                True: 0.2,
+                False: 0.6
+            }
+        }
+    }
+    normalize(probabilities)
+    s_g = 0
+    s_t = 0
+    for gene in probabilities["Harry"]["gene"]:
+        s_g += probabilities["Harry"]["gene"][gene]
+    for trait in probabilities["Harry"]["trait"]:
+        s_t += probabilities["Harry"]["trait"][trait]
+    assert s_g == 1
+    assert s_t == 1
