14745198cdb49e64e652aebe302893e1db537a37
Richmond Liew <richmond.liew.97@gmail.com>
2020-08-22 21:10:52 -0400

    working

--- Project4/nim/nim.py
+++ Project4/nim/nim.py
@@ -101,7 +101,11 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        raise NotImplementedError
+        try:
+            return self.q[(state,action)]
+        except KeyError:
+            return 0
+        
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
@@ -118,7 +122,7 @@ class NimAI():
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
-        raise NotImplementedError
+        self.q[(state,action)] = old_q + self.alpha * ((reward + future_rewards) - old_q) 
 
     def best_future_reward(self, state):
         """
@@ -130,6 +134,7 @@ class NimAI():
         Q-value in `self.q`. If there are no available actions in
         `state`, return 0.
         """
+        
         raise NotImplementedError
 
     def choose_action(self, state, epsilon=True):
